76b60f1462adcc6123ac533ef0125ec5a3205650
G1enB1and <glen.bland.81@gmail.com>
2018-10-31 18:17:48 -0500

    converted variables to objects

--- js/script.js
+++ js/script.js
@@ -1,27 +1,61 @@
 // JS (no jQuery) version
 
-var catOneNameStr = "Sprinkles";
-var catTwoNameStr = "Sparkles";
+var catOne = {
+	name:"Sprinkles",
+	image:"img/cat1.jpg",
+	clickCount:0
+};
+
+var catTwo = {
+	name:"Bandit",
+	image:"img/cat2.jpg",
+	clickCount:0
+};
+
+var catThree = {
+	name:"Snuggles",
+	image:"img/cat3.jpg",
+	clickCount:0
+};
+
+var catFour = {
+	name:"Snowflake",
+	image:"img/cat4.jpg",
+	clickCount:0
+};
+
+var catFive = {
+	name:"Pouncer",
+	image:"img/cat5.jpg",
+	clickCount:0
+};
+
+var catSix = {
+	name:"Bugaboo",
+	image:"img/cat6.jpg",
+	clickCount:0
+};
+
 var catOneElem = document.getElementById('catOneImg');
 var catTwoElem = document.getElementById('catTwoImg');
-var catOneClickCountInt = 0;
-var catTwoClickCountInt = 0;
 
-document.getElementById('catOneName').innerHTML = catOneNameStr;
-document.getElementById('catTwoName').innerHTML = catTwoNameStr;
+
+document.getElementById('catOneName').innerHTML = catOne.name;
+document.getElementById('catTwoName').innerHTML = catTwo.name;
+document.getElementById('catOneImg').src = catOne.image;
 
 catOneElem.addEventListener('click', function(){
   //the element has been clicked
   //increment click-count
-  catOneClickCountInt = catOneClickCountInt + 1;
-  document.getElementById('catOneClickCount').innerHTML = catOneClickCountInt;
+  catOne.clickCount += 1;
+  document.getElementById('catOneClickCount').innerHTML = catOne.clickCount;
 }, false);
 
 catTwoElem.addEventListener('click', function(){
   //the element has been clicked
   //increment click-count
-  catTwoClickCountInt = catTwoClickCountInt + 1;
-  document.getElementById('catTwoClickCount').innerHTML = catTwoClickCountInt;
+  catTwo.clickCount += 1;
+  document.getElementById('catTwoClickCount').innerHTML = catTwo.clickCount;
 }, false);
 
 
