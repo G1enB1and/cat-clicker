e57715a984cbcc63c661b0956dc3ad2d1a62f612
G1enB1and <glen.bland.81@gmail.com>
2018-10-31 19:06:23 -0500

    progress

--- js/script.js
+++ js/script.js
@@ -36,13 +36,45 @@ var catSix = {
 	clickCount:0
 };
 
-var catOneElem = document.getElementById('catOneImg');
-var catTwoElem = document.getElementById('catTwoImg');
-
-
 document.getElementById('catOneName').innerHTML = catOne.name;
 document.getElementById('catTwoName').innerHTML = catTwo.name;
-document.getElementById('catOneImg').src = catOne.image;
+document.getElementById('catThreeName').innerHTML = catThree.name;
+document.getElementById('catFourName').innerHTML = catFour.name;
+document.getElementById('catFiveName').innerHTML = catFive.name;
+document.getElementById('catSixName').innerHTML = catSix.name;
+
+document.getElementById('selectedCatName').innerHTML = catOne.name;
+document.getElementById('selectedCatImg').src = catOne.image;
+
+var catOneListName = document.getElementById('catOneName');
+var catTwoListName = document.getElementById('catTwoName');
+var catThreeListName = document.getElementById('catThreeName');
+var catFourListName = document.getElementById('catFourName');
+var catFiveListName = document.getElementById('catFiveName');
+var catSixListName = document.getElementById('catSixName');
+
+catOneListName.addEventListener('click', function(){
+  //the element has been clicked
+  //show the appropriate cat name, picture, and count
+  document.getElementById('selectedCatImg').src = catOne.image;
+  document.getElementById('selectedCatName').innerHTML = catOne.name;
+  //increment click-count
+  catOne.clickCount += 1;
+  document.getElementById('catOneClickCount').innerHTML = catOne.clickCount;
+}, false);
+
+catTwoListName.addEventListener('click', function(){
+  //the element has been clicked
+  //show the appropriate cat name, picture, and count
+  document.getElementById('selectedCatImg').src = catTwo.image;
+  document.getElementById('selectedCatName').innerHTML = catTwo.name;
+  //increment click-count
+  catTwo.clickCount += 1;
+  document.getElementById('catTwoClickCount').innerHTML = catTwo.clickCount;
+}, false);
+
+var catOneElem = document.getElementById('selectedCatImg');
+var catTwoElem = document.getElementById('catTwoImg');
 
 catOneElem.addEventListener('click', function(){
   //the element has been clicked
